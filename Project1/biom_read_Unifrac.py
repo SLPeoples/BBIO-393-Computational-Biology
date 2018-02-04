afa2b91e4733d0e23561909ab070cf0e398c53a6
SLPeoples <33819909+SLPeoples@users.noreply.github.com>
2018-02-04 12:37:57 -0800

    project 1 completion

--- Project1/biom_read_Unifrac.py
+++ Project1/biom_read_Unifrac.py
@@ -1,3 +1,14 @@
+"""
+biom_read_Unifrac.py
+BBIO 393 Computational Biology
+Samuel L. Peoples
+
+This script processes .biom file and creates output files and figures for use
+in exploratory analysis. Boxplots, distributions, and pcoa plots are developed
+from Jeff Gordon's A Core Gut Microbiome in Obese and Lean Twins for comparison
+of dissimilarity between Lean-vs-Lean, Obese-vs-Obese, and Lean-vs-Obese samples.
+"""
+
 from biom import load_table
 import matplotlib.pyplot as plt
 import scipy.stats as stats
@@ -5,9 +16,39 @@ import matplotlib as mpl
 import pandas as pd
 import pylab as pl
 import numpy as np
+import os.path
 import random
 import csv
 import os
+
+def checks(filepath):
+    """
+    Ensures that the program will run.
+    Checks filepaths for necessary files.
+    """
+
+    # Study directory
+    if os.path.exists(filepath+"/study_77_011618-113533"):
+        print(filepath+"/study_77_011618-113533 found.")
+    else:
+        print("Please ensure that "+filepath+"/study_77_011618-113533 is present.\n\t"
+                +"define filepath=\"path-to-study\"\n\t"
+                +"example: filepath = "+filepath)
+        exit()
+
+    # Figures directory
+    if os.path.exists(filepath+"/figures"):
+        print(filepath+"/figures found.")
+    else:
+        os.makedirs(filepath+"/figures")
+        print(filepath+"/figures created.")
+
+    # Stats directory
+    if os.path.exists(filepath+"/stats"):
+        print(filepath+"/stats found.")
+    else:
+        os.makedirs(filepath+"/stats")
+        print(filepath+"/stats created.")
 
 def single_rarefaction(filepath):
     """
@@ -17,10 +58,18 @@ def single_rarefaction(filepath):
     input = '/study_77_011618-113533/processed_data/219_otu_table.biom'
     output = '/study_77_011618-113533/processed_data/219_otu_table_even1000.biom'
     sample_size = '1000'
-    os.system('single_rarefaction.py'+
-              ' -i '+filepath+input+
-              ' -o '+filepath+output+
-              ' -d '+sample_size)
+
+    # If the rarified table is present, there is no need to run it again.
+    if os.path.exists(filepath+output):
+        pass
+    else:
+        print("Rarefaction in progress. Calling:\n\t"
+              +"single_rarefaction.py \n\t\t-i "+filepath+input
+              +' \n\t\t-o '+filepath+output+' \n\t\t-d '+sample_size)
+        os.system('single_rarefaction.py'+
+                  ' -i '+filepath+input+
+                  ' -o '+filepath+output+
+                  ' -d '+sample_size)
     return output
 
 def parse_table(filepath,rarefaction):
@@ -60,11 +109,19 @@ def betaDiversity(filepath):
     output = '/weighted_unifrac_219_otu_table_even1000.txt'
     metric = 'weighted_unifrac'
     tree = '/study_77_011618-113533/97_otus.tree'
-    os.system('beta_diversity.py'
+     # If the dissimilarity matrix is present, There is no need to run it again.
+    if os.path.exists(filepath+"/stats"+output):
+        pass
+    else:
+        print("Dissimilarity Matrix preparation in progress. Calling:\n\t"
+              +'beta_diversity.py \n\t\t-i '+filepath+input
+              +' \n\t\t-o '+filepath+output_dir+' \n\t\t-m '+metric
+              +' \n\t\t-t '+filepath+tree)
+        os.system('beta_diversity.py'
               +' -i '+filepath+input
               +' -o '+filepath+output_dir
               +' -m '+metric
-              +'-t'+filepath+tree)
+              +' -t '+filepath+tree)
     return pd.DataFrame.from_csv(filepath+output_dir+output, sep ='\t')
 
 def label_tables(filepath, betas):
@@ -136,21 +193,25 @@ def break_table(filepath, betas, lean, obese):
           "\n Mean: "+str(np.mean(LO_stats))+
           "\n Std: "+str(np.std(LO_stats))+
           "\n High: "+str(max(LO_stats))+
-          "\n Count: "+str(len(LO_stats)))
+          "\n Count: "+str(len(LO_stats))+"\n")
 
     # Save the Unifrac distances to output csvs,
+    print("Unifrac distances saved to: ")
     with open(filepath+"/stats/LL_stats_Unifrac.csv", "wb") as f:
         wr = csv.writer(f,quoting=csv.QUOTE_ALL)
         wr.writerow(LL_stats)
         f.close()
+        print("\t Lean-vs-Lean: \n\t\t"+filepath+"/stats/LL_stats_Unifrac.csv")
     with open(filepath+"/stats/OO_stats_Unifrac.csv", "wb") as f:
         wr = csv.writer(f,quoting=csv.QUOTE_ALL)
         wr.writerow(OO_stats)
         f.close()
+        print("\t Obese-vs-Obese: \n\t\t"+filepath+"/stats/OO_stats_Unifrac.csv")
     with open(filepath+"/stats/LO_stats_Unifrac.csv", "wb") as f:
         wr = csv.writer(f,quoting=csv.QUOTE_ALL)
         wr.writerow(LO_stats)
         f.close()
+        print("\t Lean-vs-Obese: \n\t\t"+filepath+"/stats/LO_stats_Unifrac.csv")
 
     return LL_stats, OO_stats, LO_stats
 
@@ -168,6 +229,7 @@ def boxplots(filepath, LL_stats, OO_stats, LO_stats):
     ax.set_xticklabels(['Lean vs Lean','Obese vs Obese', 'Lean vs Obese'])
     [flier.set(marker='o',color='#e7298a', alpha=.3) for flier in bp['fliers']]
     fig.savefig(filepath+"/figures/boxplot_unifrac.png", bbox_inches='tight')
+    print("Boxplot saved to:\n\t "+filepath+"/figures/boxplot_unifrac.png")
 
 def sample(LL_stats, OO_stats, LO_stats):
     """
@@ -193,12 +255,14 @@ def distribution(filepath,LL_stats,OO_stats,LO_stats):
     # Clear any figures that may be present.
     pl.clf()
 
+    print("Distributions saved to: ")
     # Plot the Lean vs Lean
     pl.plot(LL,LL_fit,'-o')
     pl.hist(LL,normed=True)
     pl.title("Weighted Unifrac Dissimilarity of Lean vs Lean Twins")
     pl.savefig(filepath+"/figures/LL_Distribution_Unifrac.png")
     pl.clf()
+    print("\t Lean-vs-Lean: \n\t\t "+filepath+"/figures/LL_Distribution_Unifrac.png")
 
     # Plot the Obese vs Obese
     pl.plot(OO,OO_fit,'-o')
@@ -206,20 +270,62 @@ def distribution(filepath,LL_stats,OO_stats,LO_stats):
     pl.title("Weighted Unifrac Dissimilarity of Obese vs Obese Twins")
     pl.savefig(filepath+"/figures/OO_Distribution_Unifrac.png")
     pl.clf()
-
+    print("\t Obese-vs-Obese: \n\t\t "+filepath+"/figures/OO_Distribution_Unifrac.png")
     #Plot the Lean vs Obese
     pl.plot(LO,LO_fit,'-o')
     pl.hist(LO,normed=True)
     pl.title("Weighted Unifrac Dissimilarity of Lean vs Obese Twins")
     pl.savefig(filepath+"/figures/LO_Distribution_Unifrac.png")
     pl.clf()
+    print("\t Lean-vs-Obese: \n\t\t "+filepath+"/figures/LO_Distribution_Unifrac.png")
 
-def main():
+def pcoa(filepath):
+    """
+    Runs principal_coordinates.py and make_emperor.py
+    to create an html file which allows the user to visualize
+    the differences between the obesity categories
+    """
+    input = "/stats/weighted_unifrac_219_otu_table_even1000.txt"
+    output = "/stats/weighted_unifrac_219_otu_table_even1000_pcoa.txt"
+
+    # If the pcoa file is present, there is no need to run it again.
+    if os.path.exists(filepath+output):
+        pass
+    else:
+        print("PCoA in progress. Calling:\n\t"
+              +"principal_coordinates.py \n\t\t-i "+filepath+input
+              +' \n\t\t-o '+filepath+output)
+        os.system('principal_coordinates.py'+
+                  ' -i '+filepath+input+
+                  ' -o '+filepath+output)
+    print("Principal coordinates saved to: \n\t"+filepath+output)
+
+    # If the html file is present, there is no need to run it again.
+    input = output
+    mapping = "/study_77_011618-113533/mapping_files/2485_mapping_file.txt"
+    coloring = "obesitycat"
+    output_dir = "/figures"
+    output = "/figures/index.html"
+    if os.path.exists(filepath+output):
+        pass
+    else:
+        print("Emperor visualization in progress. Calling:\n\t"
+              +"make_emperor.py \n\t\t-i "+filepath+input
+              +' \n\t\t-o '+filepath+output)
+        os.system('principal_coordinates.py'+
+                  ' -i '+filepath+input+
+                  ' -m '+filepath+mapping+
+                  ' -b '+coloring+
+                  ' -o '+filepath+output_dir)
+    print("Principal coordinates saved to: \n\t"+filepath+output)
+
+def main(filepath = "/home/qiime/Desktop/Project1"):
     """
     Define the path to the script, which should be in
     the same directory as study_77_011618-113533
     """
-    filepath = '/home/qiime/Desktop/Project1'
+
+    checks(filepath)
 
     # Rarefaction of biom table, assigns path as variable
     rarefaction = single_rarefaction(filepath)
@@ -237,6 +343,9 @@ def main():
 
     # Plot the Distributions
     distribution(filepath,LL_stats,OO_stats,LO_stats)
+
+    # Plot the scatter
+    pcoa(filepath)
 
 if __name__ == '__main__':
     main()
